74e28b57c181151317bb03f36c9634c533d2ad7c
s1107777 <58693732+s1107777@users.noreply.github.com>
2020-01-24 15:37:08 +0100

    opdracht af

---
+++ imtue-weekopdracht-accessibility-s1107777-master/main.js
@@ -0,0 +1,47 @@
+// functionality for showing/hiding the comments section
+
+const showHideBtn = document.getElementById('js--show-hide');
+const commentWrapper = document.getElementById('js--comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// functionality for adding a new comment via the comments form
+
+const form = document.getElementById('js--comment-form');
+const nameField = document.getElementById('js--name');
+const commentField = document.getElementById('js--comment');
+const list = document.getElementById('js--comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
